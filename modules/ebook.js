f3eeb782f6d41082f6415b8f653db149a3c86d9d
feather <paigetech@gmail.com>
2014-12-12 10:43:48 -0800

    working set

--- modules/ebook.js
+++ modules/ebook.js
@@ -15,15 +15,23 @@ var dateNow = function() {
   var yy = dateNow.getFullYear().toString();
   if (dd < 10) {
     dd = "0" + dd;
-  };
+  }
   return (yy + '-' + mm + '-' + dd);
 };
 
 function doIt(){
 
-  //var url = "https://www.federalregister.gov/articles/2013/08/19/2013-18956/medicare-program-hospital-inpatient-prospective-payment-systems-for-acute-care-hospitals-and-the";
-  var url = "https://www.federalregister.gov/articles/2014/11/10/2014-26146/medicare-and-medicaid-programs-hospital-outpatient-prospective-payment-and-ambulatory-surgical";
+  //var collection = "mre_paige_test";
+  //opps
+  var collection = "mre_pps_outpt2015";
+  //pfs
+  //var collection = "mre_pps_pfs2015";
 
+  //OPPS
+  var url = "https://www.federalregister.gov/articles/2014/11/10/2014-26146/medicare-and-medicaid-programs-hospital-outpatient-prospective-payment-and-ambulatory-surgical";
+  //PFS
+  //var url = "https://www.federalregister.gov/articles/2014/11/13/2014-26183/medicare-program-revisions-to-payment-policies-under-the-physician-fee-schedule-clinical-laboratory";
+  console.log("Downloading URL: " + url);
   var fileName = 'public/temp.html';
 
   request(url, function(error, response, html){
@@ -31,6 +39,8 @@ function doIt(){
       var $ = cheerio.load(html);
       var source = $('.article').html();
 
+      var FRVolume = $('.volume').html();
+      var edition = $('.page').html();
 
       //building the doc
       var doc = "";
@@ -39,7 +49,7 @@ function doIt(){
 
       //body Summary and Preface
       //uid
-      var uid = "uid"; //need to set up
+      var uid = FRVolume + "FR" + edition; //need to set up
 
       //date
       var dateBody = $('.metadata_list').html();
@@ -170,8 +180,18 @@ function doIt(){
 
         doc += agencyDocet + '\n';
 
+        //setup the short title
+        var shortTitle = matches;
+        re = /CMS-\d+-(\w+)/g;
+        var extension = re.exec(shortTitle)[1];
+        if (extension === "FC") {
+          shortTitle += " Final Rule for 2015 OPPS & ASC";
+        } else if (extension === "P") {
+          shortTitle += " Proposed Rule for 2015 OPPS & ASC";
+        }
+
         //we now have the info to build the h2
-        var h2 = "<doc>\n<h2>" + title + " " + matches  + "</h2>\n:::uid " + uid + "0\n</doc>\n";
+        var h2 = "<doc>\n<h2>" + shortTitle + "</h2>\n:::uid " + uid + "0\n</doc>\n";
 
         doc = h2 + doc;
 
@@ -195,135 +215,196 @@ function doIt(){
         doc += "<br><p><b>Shorter URL:</b> <a href=\"" + shortURL +"\">" + shortURL + "</a></p>";
 
         //Find the TOC so that we can remove it while saving it for later
-//        var toc = $('#content_area #table_of_contents').html();
-//      toc += $('#content_area .table_of_contents').html();
-//
-//      var tables = $('#content_area #table_of_tables').html();
-//      tables += $('#content_area .table_of_tables').html();
+      var toc = $('#content_area #table_of_contents').html();
+      toc += $('#content_area .table_of_contents').html();
+
+      var tables = $('#content_area #table_of_tables').html();
+      tables += $('#content_area .table_of_tables').html();
       //remove the TOC and table of tables
       $('#content_area .table_of_contents').remove();
       $('#content_area #table_of_contents').remove();
       $('#content_area .table_of_tables').remove();
       $('#content_area #table_of_tables').remove();
-//
-//      //this is in the wrong stinking place
-//      doc += "\n<\/doc>\n<doc>\n:::uid tableofcontents\n<h3>Table of Contents</h3>" + toc + "\n<\/doc>";
 
-        //body info for the rest of the build
-        var body = $('#content_area').html();
+      //this is in the wrong stinking place
+      var tableOfContents = "\n<doc>\n:::uid tableofcontents" + FRVolume + "\n<h3>Table of Contents</h3>" + toc + "\n<\/doc>";
+      //remove the awkward Back to Top
+      re = /Table of Contents <a href="#table_of_contents" class="back_to_top">Back to Top<\/a>/g;
+      tableOfContents = tableOfContents.replace(re, "");
+      //change the LI out for p's with class
+      re = /<li\s/g;
+      tableOfContents = tableOfContents.replace(re, "<p ");
+      re = /<\/li>/g;
+      tableOfContents = tableOfContents.replace(re, "<\/p>");
 
-        //adding indexs and alinks
-        re = /<span\sclass="printed_page"\sid="page-(\d+)[\S\s]*?<\/span>/g;
-        body = body.replace(re, "\n:::index 79p$1\n<a name=\"79p$1\"><\/a>\n");
+      //change the a links into uids
+      re = /<a\shref=\"#(\w+)-(\d+)\">/g;
+      //replace = "<!!uf " + collection + " " + FRVolume + edition + "$1$2\">";
+      replace = "<a href=\"" + FRVolume + edition + "$1$2\"><\/a>";
+      tableOfContents = tableOfContents.replace(re, replace);
 
-        //getting rid of back to top
-        re = /<a href="[#_\w]+"\sclass="back_to_top">Back to Top<\/a>/g;
-        body = body.replace(re, "");
+      //body info for the rest of the build
+      var body = $('#content_area').html();
 
-        //get rid of divs
-        re = /<div\s[\w="_\s]+>/g;
-        body = body.replace(re, "");
-        re = /<\/div>/g;
-        body = body.replace(re, "");
+      //adding indexs and alinks
+      re = /<span\sclass="printed_page"\sid="page-(\d+)[\S\s]*?<\/span>/g;
+      body = body.replace(re, "\n:::index 79p$1\n<a name=\"79p$1\"><\/a>\n");
 
-        //get rid of h1s
-        re = /<h1[\S\s]*?>/g;
-        body = body.replace(re, "<br><br><p><b>");
-        re = /<\/h1>/g;
-        body = body.replace(re, "<\/b><\/p>");
+      //getting rid of back to top
+      re = /<a href="[#_\w]+"\sclass="back_to_[\w_]+">Back to Top<\/a>/g;
+      body = body.replace(re, "");
 
-        //setup the different docs
+      //get rid of divs
+      re = /<div\s[\w="_\s]+>/g;
+      body = body.replace(re, "");
+      re = /<\/div>/g;
+      body = body.replace(re, "");
 
-        //first get rid of the h4s we don't want
-        re = /<h4[\S\s]*?>/g;
-        body = body.replace(re, "<br><p><b>");
-        re = /<\/h4>/g;
-        body = body.replace(re, "<\/b><\/p>");
+      //get rid of h1s
+      re = /<h1[\S\s]*?>/g;
+      body = body.replace(re, "<br><br><p><b>");
+      re = /<\/h1>/g;
+      body = body.replace(re, "<\/b><\/p>");
 
-        //get rid of the plain h3s
-        re = /<h3>([\S\s]*?)<\/h3>/g;
-        body = body.replace(re, "<p><b>$1<\/b><\/p>\n");
+      //setup the different docs
 
+      //first get rid of the h4s we don't want
+      re = /<h4[\S\s]*?>/g;
+      body = body.replace(re, "<br><p><b>");
+      re = /<\/h4>/g;
+      body = body.replace(re, "<\/b><\/p>");
 
-        //h3s -> h4s for docs
-        re = /<h3[\S\s]*?id=\"\w\-(\d+)\"[\S\s]*?>/g;
-        replace = "\n<\/doc>\n<doc>\n:::date " + date + "\n:::uid " + uid + "$1\n<link rel=\"stylesheet\" type=\"text/css\" href=\"http://portal.mediregs.com/globaltext.css\">\n<link rel=\"stylesheet\" type=\"text/css\" href=\"http://portal.mediregs.com/fedreg.css\">\n<h4><a name=\"$1\"><\/a>";
-        body = body.replace(re, replace);
-        re = /<\/h3>/g;
-        body = body.replace(re, "<\/h4>\n");
-
-        //look for h3's that should be docs in their own right
-        re = /<doc>\n:::uid\s([\w\d]+)\n<h3>([IVX]+\.)([\w\d\s\.\-\:\(\)\\\/]+)<\/h3>[\n\s]+?<p/g;
-        replace = "\n<\/doc>\n<doc>\n:::uid " + uid + "head$1\n<h3>$2$3<\/h3>\n\n<\/doc>\n<doc>\n:::date " + date + "\n:::uid $1\n<link rel=\"stylesheet\" type=\"text/css\" href=\"http://portal.mediregs.com/globaltext.css\">\n<link rel=\"stylesheet\" type=\"text/css\" href=\"http://portal.mediregs.com/fedreg.css\">\n<h4>$3<\/h4>\n<p";
-        body = body.replace(re, replace);
-
-        //catpture the important h2s
-        re = /<h2\sid=\"([\w\d\"\-\s=]+)\"[\w\d\"\-\s=]+\">([IVX]+\.[\S\s]*?)<\/h2>/g;
-        replace = "\n<\/doc>\n<doc>\n:::uid " + uid + "$1\n<h3>$2<\/h3>\n";
-        body = body.replace(re, replace);
-        //get rid of the others
-        re = /<h2\sid=\"([\w\d\"\-\s=]+)\"[\w\d\"\-\s=]+\">([\S\s]*?)<\/h2>/g;
-        replace = "\n<p><b>$2<\/b><\/p>\n";
-        body = body.replace(re, replace);
+      //get rid of the plain h3s
+      re = /<h3>([\S\s]*?)<\/h3>/g;
+      body = body.replace(re, "<p><b>$1<\/b><\/p>\n");
 
 
-        //setup for Footnotes
-        re = /<p><b>Footnotes/g;
-        replace = "\n<\/doc>\n<doc>\n:::date " + date +  "\n:::uid " + uid + "footnotes\n<link rel=\"stylesheet\" type=\"text/css\" href=\"http://portal.mediregs.com/globaltext.css\">\n<link rel=\"stylesheet\" type=\"text/css\" href=\"http://portal.mediregs.com/fedreg.css\">\n<h3>Footnotes<\/h3>\n";
-        body = body.replace(re, replace);
-        re = /<div[\s\w\d=_\-\"]+class="footnote">/g;
-        replace = "";
-        body = body.replace(re, replace);
+      //h3s -> h4s for docs
+      re = /<h3[\S\s]*?id=\"(\w\-\d+)\"[\S\s]*?>/g;
+      replace = "\n<\/doc>\n<doc>\n:::date " + date + "\n:::uid " + uid + "$1\n<link rel=\"stylesheet\" type=\"text/css\" href=\"http://portal.mediregs.com/globaltext.css\">\n<link rel=\"stylesheet\" type=\"text/css\" href=\"http://portal.mediregs.com/fedreg.css\">\n<h4><a name=\"$1\"><\/a>";
+      body = body.replace(re, replace);
+      re = /<\/h3>/g;
+      body = body.replace(re, "<\/h4>\n");
 
-        //setup for List of Subjects
-        re = /<p><b>List of Subjects/g;
-        replace = "\n<\/doc>\n<doc>\n:::uid " + uid + "headlistofsubject\n<h3>List of Subjects<\/h3>\n\n<\/doc>\n<doc>\n:::date " + date +  "\n:::uid " + uid + "listofsubjects\n<link rel=\"stylesheet\" type=\"text/css\" href=\"http://portal.mediregs.com/globaltext.css\">\n<link rel=\"stylesheet\" type=\"text/css\" href=\"http://portal.mediregs.com/fedreg.css\">\n<h4>List of Subjects<\/h4>\n";
-        body = body.replace(re, replace);
+      //look for h3's that should be docs in their own right
+      re = /<doc>\n:::uid\s([\w\d]+)\n<h3>([IVX]+\.)([\w\d\s\.\-\:\(\)\\\/]+)<\/h3>[\n\s]+?<p/g;
+      replace = "\n<\/doc>\n<doc>\n:::uid " + uid + "head$1\n<h3>$2$3<\/h3>\n\n<\/doc>\n<doc>\n:::date " + date + "\n:::uid $1\n<link rel=\"stylesheet\" type=\"text/css\" href=\"http://portal.mediregs.com/globaltext.css\">\n<link rel=\"stylesheet\" type=\"text/css\" href=\"http://portal.mediregs.com/fedreg.css\">\n<h4>$3<\/h4>\n<p";
+      body = body.replace(re, replace);
 
-        //clean up the rest of the h's
-        re = /<h5[\S\s]*?>/g;
-        body = body.replace(re, "<br><p><b>");
-        re = /<\/h5>/g;
-        body = body.replace(re, "<\/b><\/p>");
-
-        //setup the PARTS
-        re = /<p><b>(PART\s(\d+)[\S\s]*?)<\/b><\/p>/g;
-        replace = "\n<\/doc>\n<doc>\n:::date " + date + "\n:::uid " + uid + "part$2\n<link rel=\"stylesheet\" type=\"text/css\" href=\"http://portal.mediregs.com/globaltext.css\">\n<link rel=\"stylesheet\" type=\"text/css\" href=\"http://portal.mediregs.com/fedreg.css\">\n<h4>$1<\/h4>";
-        body = body.replace(re, replace);
-
-        //clean up double end docs
-        re = /<\/doc>\n+<\/doc>/g;
-        body = body.replace(re, "<\/doc>");
-
-        //clean up random h2
-        re = /<h2([\s\w\d=\"_]+)?><\/h2>/g;
-        body = body.replace(re, "");
-
-        //get rid of "back to context"
-//        re = /<a[\S\s]*?>Back to Context([\n\s]+)?<\/a>/g;
-//      body = body.replace(re, "");
-//      //get rid of "back to top"
-//      re = /<a[\S\s]*?>Back to Top([\n\s]+)?<\/a>/g;
-//      body = body.replace(re, "");
-//
-//      //get rid of "back to top"
-//      re = /\n<\/h(\d)>/g;
-//      body = body.replace(re, "<\/h$1>");
+      //catpture the important h2s
+      re = /<h2\sid=\"([\w\d\"\-\s=]+)\"[\w\d\"\-\s=]+\">([IVX]+\.[\S\s]*?)<\/h2>/g;
+      replace = "\n<\/doc>\n<doc>\n:::uid " + uid + "$1\n<h3>$2<\/h3>\n";
+      body = body.replace(re, replace);
+      //get rid of the others
+      re = /<h2\sid=\"([\w\d\"\-\s=]+)\"[\w\d\"\-\s=]+\">([\S\s]*?)<\/h2>/g;
+      replace = "\n<p><b>$2<\/b><\/p>\n";
+      body = body.replace(re, replace);
 
 
-        doc += body;
-        //fix uids
-        re = /(:::uid\s[\w\d]+)\-([\w\d]+)/g;
-        doc = doc.replace(re, "$1$2"); 
+      //setup for Footnotes
+      re = /<p><b>Footnotes/g;
+      replace = "\n<\/doc>\n<doc>\n:::date " + date +  "\n:::uid " + uid + "footnotes\n<link rel=\"stylesheet\" type=\"text/css\" href=\"http://portal.mediregs.com/globaltext.css\">\n<link rel=\"stylesheet\" type=\"text/css\" href=\"http://portal.mediregs.com/fedreg.css\">\n<h3>Footnotes<\/h3>\n";
+      body = body.replace(re, replace);
+      re = /<div[\s\w\d=_\-\"]+class="footnote">/g;
+      replace = "";
+      body = body.replace(re, replace);
 
-        doc += "\n<\/doc>";
-        console.log("This is the doc" + doc);
+      //setup for List of Subjects
+      re = /<p><b>List of Subjects/g;
+      replace = "\n<\/doc>\n<doc>\n:::uid " + uid + "headlistofsubject\n<h3>List of Subjects<\/h3>\n\n<\/doc>\n<doc>\n:::date " + date +  "\n:::uid " + uid + "listofsubjects\n<link rel=\"stylesheet\" type=\"text/css\" href=\"http://portal.mediregs.com/globaltext.css\">\n<link rel=\"stylesheet\" type=\"text/css\" href=\"http://portal.mediregs.com/fedreg.css\">\n<h4>List of Subjects<\/h4>\n";
+      body = body.replace(re, replace);
 
-        //what is in the doc now
-        fs.writeFileSync("xpathTest.html", doc);
-        //console.log(doc);
-    }
-  });
+      //clean up the rest of the h's
+      re = /<h5[\S\s]*?>/g;
+      body = body.replace(re, "<br><p><b>");
+      re = /<\/h5>/g;
+      body = body.replace(re, "<\/b><\/p>");
+
+      //setup the PARTS
+      re = /<p><b>(PART\s(\d+)[\S\s]*?)<\/b><\/p>/g;
+      replace = "\n<\/doc>\n<doc>\n:::date " + date + "\n:::uid " + uid + "part$2\n<link rel=\"stylesheet\" type=\"text/css\" href=\"http://portal.mediregs.com/globaltext.css\">\n<link rel=\"stylesheet\" type=\"text/css\" href=\"http://portal.mediregs.com/fedreg.css\">\n<h4>$1<\/h4>";
+      body = body.replace(re, replace);
+
+      //clean up double end docs
+      re = /<\/doc>\n+<\/doc>/g;
+      body = body.replace(re, "<\/doc>");
+
+      //clean up random h2
+      re = /<h2([\s\w\d=\"_]+)?><\/h2>/g;
+      body = body.replace(re, "");
+
+      //add the toc
+      //doc += tableOfContents;
+      //add the body to the prepared doc
+      doc += body;
+      //fix uids
+      re = /(:::uid\s[\w\d]+)\-([\w\d]+)/g;
+      doc = doc.replace(re, "$1$2"); 
+
+      //run the find and replace set
+      re = /<a href=\"\/citation\/(([\d]+)-FR-[\d]+)\">/g;
+      replace = "<!!ln dp_fr$2 $1p$2 #$1p$2>";
+      doc = doc.replace(re, replace);
+
+      re = /<a[\s\w\d\-\"#=]+rel=\"footnote\">([\s\n]+\[\d+\][\s\n]+)<\/a>/g;
+      replace = "\n$1\n";
+      doc = doc.replace(re, replace);
+
+      //take care of any images
+      re = /<p\sclass="graphic"><a[\s\w\"\-=]+href="https:\/\/s3\.amazonaws\.com\/images\.federalregister\.gov\/([A-Z0-9\.]+)\/\w+(\.\w{3})\"[\s\w\"\-=]+><img[\s\w\"\-=:\/\.]+><\/a><\/p>/g;
+      replace = "<p class=\"graphic\"><!!img><img src=\"$1$2\"><\/a><\/p>";
+      doc = doc.replace(re, replace);
+
+      //get rid of classes
+      re = /\sclass=\"([\w\d\-_\s]+)?\"/g;
+      doc = doc.replace(re, "");
+
+      //change the alinks in headers
+      re = /(<h\d>)<a\sname=\"(\w)-(\d+)\"><\/a>([\S\s]*?<\/h\d>)/g;
+      replace = "$1$4\n<a name=\"" + FRVolume + edition + "$2$3\"><\/a>";
+      doc = doc.replace(re, replace);
+
+      //header id removal
+      re = /(<h\d)\s[\w\s\d=\"]+>/g;
+      replace = "$1>";
+      doc = doc.replace(re, replace);
+
+
+      //add in the toc
+      re = /(<\/doc>)\n(<doc>\n:::uid\s[\w\d]+\n<h3>I\.[\w\d\s]+\n<\/h3>)/;
+      replace = "$1\n" + tableOfContents + "\n$2";
+      doc = doc.replace(re, replace);
+
+
+
+      //download the images we'll need
+      var loadedFile = source;
+
+      re = /src=\"(https:\/\/s\d\.amazonaws\.com\/images\.federalregister\.gov\/.*?\/\w+\.\w{3})\"/ig;
+
+      var captures = [];
+      while (match = re.exec(loadedFile)) {
+            captures.push(match[1]);
+      }
+
+      captures.forEach(function(url) {
+        var re = /https:\/\/s\d\.amazonaws\.com\/images\.federalregister\.gov\/([\d\w\.]+)\/\w+\.\w{3}/ig;
+        var fileVar = re.exec(url);
+        var fileName = "images/" + fileVar[1].toLowerCase() + '.png';
+
+        request(url).pipe(fs.createWriteStream(fileName));
+
+      });
+      console.log('complete');
+     
+      doc += "\n<\/doc>";
+      console.log("Done");
+
+      //what is in the doc now
+      fs.writeFileSync("xpathTest.html", doc);
+      //console.log(doc);
+  }
+});
 
 }
 
